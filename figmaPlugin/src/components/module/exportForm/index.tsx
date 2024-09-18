75ba433a80ccd44621ad3b856d7c5a8b5f081de0
wgfire <364049242@qq.com>
2024-09-18 21:22:26 +0800

    refactor(figma): 优化表单结构

--- figmaPlugin/src/components/module/exportForm/index.tsx
+++ figmaPlugin/src/components/module/exportForm/index.tsx
@@ -3,14 +3,61 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import {CascadeSelect} from '@/components/ui/cascadeSelect'
+import { CascadeSelect } from "@/components/ui/cascadeSelect";
 export interface FormValues {
   id: string;
   name: string;
-  pageType?: "STATIC_IMAGE" | "GRAPHIC_SITE";
-  format?: "PNG" | "SVG" | "PDF" | "JPG";
-  i18n?: "zh" | "en";
+  renders?: Render[];
+  format?: "PNG" | "JPG";
 }
+// 设备对象接口
+interface Render {
+  type: DeviceType; // 设备类型，如PC，iPad，H5
+  pageType: PageType; // 页面类型，如静态图片，图文并茂
+  languagePageMap: Map<Language, Object>;
+}
+enum DeviceType {
+  PC = "pc",
+  IPad = "ipad",
+  H5 = "h5",
+}
+// 页面类型枚举
+enum PageType {
+  StaticImage = "STATIC_IMAGE",
+  GraphicSite = "GRAPHIC_SITE",
+}
+
+// 语言类型枚举
+enum Language {
+  English = "en",
+  Chinese = "cn",
+  Spanish = "es", //可以扩展其他语言
+}
+
+const devicesData: { label: keyof typeof DeviceType; value: DeviceType }[] = [
+  {
+    label: "PC",
+    value: DeviceType.PC,
+  },
+  {
+    label: "IPad",
+    value: DeviceType.IPad,
+  },
+  {
+    label: "H5",
+    value: DeviceType.H5,
+  },
+];
+const pageTypeData: { label: string; value: PageType }[] = [
+  {
+    label: "静态图片",
+    value: PageType.StaticImage,
+  },
+  {
+    label: "图文并茂",
+    value: PageType.GraphicSite,
+  },
+];
 
 interface FormProps {
   defaultValues: FormValues;
@@ -20,9 +67,7 @@ interface FormProps {
 
 export const ExportForm: React.FC<FormProps> = ({ defaultValues, values, onChange }) => {
   const [formValues, setFormValues] = useState<FormValues>({
-    pageType: "STATIC_IMAGE",
     format: "PNG",
-    i18n: "zh",
     ...defaultValues,
   });
   console.log(formValues, "values", defaultValues);
@@ -47,19 +92,36 @@ export const ExportForm: React.FC<FormProps> = ({ defaultValues, values, onChang
           <Label htmlFor="pageId">页面ID</Label>
           <Input id="pageId" value={formValues.id} disabled onChange={(e) => handleChange("id", e.target.value)} />
         </div>
-        <div className="space-y-2">
+        <div className="space-y-2 w-full col-start-1 col-span-2">
           <Label htmlFor="pageName">页面名称</Label>
           <Input id="pageName" placeholder="Enter page name" value={formValues.name} onChange={(e) => handleChange("name", e.target.value)} />
         </div>
-        <div className="space-y-2">
-          <Label htmlFor="pageTypes">页面类型</Label>
-          <Select value={formValues.pageType} onValueChange={(value) => handleChange("pageType", value as FormValues["pageType"])}>
-            <SelectTrigger id="pageTypes">
+        <div className="space-y-2 w-full ">
+          <Label htmlFor="deviceType">设备类型</Label>
+          <Select value={formValues.renders} onValueChange={(value) => handleChange("renders", value as unknown as FormValues["renders"])}>
+            <SelectTrigger id="deviceType">
               <SelectValue placeholder="Select type" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="STATIC_IMAGE">静态图片</SelectItem>
-              <SelectItem value="GRAPHIC_SITE" disabled>
+              <SelectItem value="STATIC_IMAGE">Pc</SelectItem>
+              <SelectItem value="GRAPHIC_SITE" >
+                Ipad
+              </SelectItem>
+              <SelectItem value="GRAPHIC_SITE" >
+                H5
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="space-y-2 w-full ">
+          <Label htmlFor="pageType">页面类型</Label>
+          <Select>
+            <SelectTrigger id="pageType">
+              <SelectValue placeholder="Select type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value={PageType.StaticImage}>静态站点</SelectItem>
+              <SelectItem value={PageType.GraphicSite} disabled>
                 图文并茂
               </SelectItem>
             </SelectContent>
